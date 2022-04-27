30de0a83c78d7b57f309190601a002df0e70a2b6
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-04-27 15:47:52 -0600

    TEST ROUTES FOR users#index & users#show

---
+++ test/integration/routes_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class RoutesTest < ActionController::TestCase
+  test "route test users#index" do
+    assert_generates '/users', controller: 'users', action: 'index'
+  end
+
+  test "route test users#show" do
+    assert_generates '/users/1', { controller: 'users', action: 'show', id: '1' }
+  end
+end
