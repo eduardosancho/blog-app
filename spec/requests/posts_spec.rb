f0477bc980a685a123ee6555fcc46baa9c976d48
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-04-28 14:02:29 -0600

    TEST posts #show & #index GET REQUESTS

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,41 @@ require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:example) {
+      user_index = rand(1..5)
+      get user_posts_path(user_index) 
+    } # get(:index)
+
+    it "request is a success" do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'index' view html" do
+      expect(response).to render_template(:index)
+    end
+
+    it "response body has expected title text" do
+      expect(response.body).to include('Posts Index')
+    end
+  end
+
+  describe "GET /show" do
+    before(:example) {
+      user_index = rand(1..5)
+      post_index = rand(1..5)
+      get user_post_path(user_index, post_index) 
+    } # get(:show)
+
+    it "request is a success" do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'show' view html" do
+      expect(response).to render_template(:show)
+    end
+
+    it "response body has expected title text" do
+      expect(response.body).to include('Post Details')
+    end
   end
 end
