520019cfe56df40b58b3278e397c969e6a2979cd
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-04-28 13:49:07 -0600

    TEST HTTP get users#show

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -16,4 +16,23 @@ RSpec.describe "Users", type: :request do
       expect(response.body).to include('Users Index')
     end
   end
+
+  describe "GET /show" do
+    before(:example) { 
+      user_index = rand(1..5)
+      get user_path(user_index) 
+    } # get(:show)
+
+    it "request is a success" do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'show' view html" do
+      expect(response).to render_template(:show)
+    end
+
+    it "response body has expected title text" do
+      expect(response.body).to include('User Profile')
+    end
+  end
 end
