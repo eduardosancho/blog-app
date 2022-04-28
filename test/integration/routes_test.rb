dfb4edcf226ba76b1e2df840706c81ea814349e9
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-04-27 19:57:21 -0600

    ADD TESTS FOR posts ROUTES

--- test/integration/routes_test.rb
+++ test/integration/routes_test.rb
@@ -8,4 +8,12 @@ class RoutesTest < ActionController::TestCase
   test 'route test users#show' do
     assert_generates '/users/1', { controller: 'users', action: 'show', id: '1' }
   end
+
+  test 'route test posts#index' do
+    assert_generates '/users/1/posts', { controller: 'posts', action: 'index', user_id: '1' }
+  end
+
+  test 'route test posts#show' do
+    assert_generates '/users/1/posts/1', { controller: 'posts', action: 'show', user_id: '1', id: '1' }
+  end
 end
