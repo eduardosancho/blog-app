c263ee04466b842aeefec53351442f6315ac5ea0
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-03 19:27:09 -0600

    Rename foreign_key to 'author_id' referencing to users in posts, likes and comments tables.

--- db/migrate/20220428233150_add_user_ref_to_posts.rb
+++ db/migrate/20220428233150_add_user_ref_to_posts.rb
@@ -1,5 +1,6 @@
 class AddUserRefToPosts < ActiveRecord::Migration[7.0]
   def change
-    add_reference :posts, :user, null: false, foreign_key: true
+    add_reference :posts, :author, null: false, index:true
+    add_foreign_key :posts, :users, column: :author_id
   end
 end
