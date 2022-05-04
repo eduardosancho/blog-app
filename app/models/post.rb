dbb23272eb4e32a7c87f1be7e755a3cc22d34889
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-03 22:24:52 -0600

    Set up association post has_many likes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments, :dependent => :destroy
+  has_many :likes, :dependent => :destroy
 
   before_save :update_posts_counter
 
