86e57581e14093cae67ca8f0e4aabfeb4f078623
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-03 19:45:05 -0600

    Set up association user has_many comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,3 @@
 class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
 end
