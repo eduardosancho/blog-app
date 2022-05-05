e4712da0b0d1548b365487be40d6e7a3b5e7dfce
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-05 00:21:41 -0600

    Add config timezone into application.rb

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module BlogApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = ‘Eastern Time (US & Canada)'`.
   end
 end
