c4338bade668544a1672677a98e1004fd9def895
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-05 11:15:37 -0600

    Fix typo in config.time_zone

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,6 @@ module BlogApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.time_zone = ‘Eastern Time (US & Canada)'`.
+    config.time_zone = 'Eastern Time (US & Canada)'
   end
 end
