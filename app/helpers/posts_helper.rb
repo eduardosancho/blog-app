e7e4245edc131e9801a7e7c2f3b12b74a14449cf
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-04 23:13:50 -0600

    Add has_comments? helper to remove 'if' from views

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,9 @@
 module PostsHelper
+  def has_comments?(post)
+    out = ''
+    if post.comments_counter.nil?
+      out << '<p>This post has no comments</p>'
+    end
+    out.html_safe
+  end
 end
