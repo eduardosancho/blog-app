fe888aabaf21fedcf0724119a19322e2b4ed63af
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-06 15:37:20 -0600

    Add test for user#create validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations Testing' do
+    subject { User.new(name: 'Marito Mortadela', posts_counter: 0) }
+
+    before { subject.save }
+
+    it 'should be valid if the name contains text' do
+      expect(subject).to be_valid
+    end
+
+    it 'should not be valid if the name is nil' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'should be valid if posts_counter is 0' do
+      expect(subject).to be_valid
+    end
+
+    it 'should not be valid if posts_counter is string' do
+      subject.posts_counter = 'hello'
+      expect(subject).to_not be_valid
+    end
+
+    it 'should not be valid if posts_counter is -2' do
+      subject.posts_counter = -2
+      expect(subject).to_not be_valid
+    end
+
+    it 'should return three posts when calling most_recent_posts' do
+      (1..7).each do
+        Post.create(title: 'hello!', author_id: subject.id, comments_counter: 0, likes_counter: 0)
+      end
+
+      expect(subject.recent_posts.length).to eq(3)
+    end
+  end
 end
