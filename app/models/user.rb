f2cf549e6385e6b8a1d89ef1ea16e6b03f2ac668
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-06 00:14:55 -0600

    Validate name and posts_counter for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id', dependent: :destroy
   has_many :likes, foreign_key: 'author_id', dependent: :destroy
 
+  validates_presence_of :name
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def recent_posts
     posts.limit(3).order(created_at: :desc)
   end
