345feb1c5d5433cb0c7b0c424cc50823f336c6f7
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-06 15:38:51 -0600

    Add test for likes#create validations

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Likes Validations' do
+    it 'should increase it\'s post likes_counter by 1 uppon creation' do
+      user = User.new(name: 'Arthur Morgan', posts_counter: 0)
+      user.save
+
+      post = Post.new(title: 'Red Dead', text: 'yehaa', comments_counter: 0, likes_counter: 0, author_id: user.id)
+      post.save
+
+      like = Like.new(author_id: user.id, post_id: post.id)
+      like.save
+
+      expect(like.post.likes_counter).to eq(1)
+    end
+  end
 end
