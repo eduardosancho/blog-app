5535f54134ae8471f5bd1410041ac6018f167fa9
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-05 19:50:26 -0600

    Enable add like for specific post

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,18 @@
 class LikesController < ApplicationController
+  def create
+    owner = User.find(params[:user_id])
+    post = Post.find(params[:post_id])
+    @like = Like.new
+    @like.author_id = current_user.id
+    @like.post_id = params[:post_id]
+    respond_to do |format|
+      format.html do
+        if @like.save
+          flash[:success] = 'Like saved succesfully'
+        else
+          flash[:error] = 'Error: Like could not be saved'
+        end
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :new, :create, :show] do
       resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
     end
   end
 
