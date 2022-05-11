2689d48bcc93126fa4f67d42af14682f40e1790d
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-11 11:52:27 -0600

    Implement destroy comment functionality

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :new, :create, :show, :destroy] do
-      resources :comments, only: [:new, :create]
+      resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:create]
     end
   end
