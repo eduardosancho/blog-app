036850cc2546714b22fafd2f8d611cff5241a6fb
Bondok6 <kirolousbondok6@gmail.com>
2022-05-13 01:12:23 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,11 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users
+      resources :users do
+        resources :posts do
+          resources :comments
+        end
+      end
     end
   end
 
