335d2236064c589b08ac44be18b901a8f5d37686
Bondok6 <kirolousbondok6@gmail.com>
2022-05-12 23:58:18 +0200

    Add api endpoint for users#index

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :set_user
+  before_action :set_user, only: [:show]
+
+  def index
+     @users = User.all 
+     render json: @users
+  end
 
   def show
     render json: @user.to_json(include: [:posts])
